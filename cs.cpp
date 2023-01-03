7741e878496e0fa962034c7a2229b1ac3ed99b34
SanskarBhadani <115225957+Sanskar-cloud@users.noreply.github.com>
2023-01-03 19:46:31 +0530

    Add files via upload

---
+++ cs.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+    vector<int> findOrder(int n, int m, vector<vector<int>> prerequisites) 
+    {
+        //code here
+        vector<int> adj [n];
+        for(auto &e : prerequisites){
+            adj[e[1]].push_back(e[0]);
+        }
+	    vector<int> inDegree(n, 0);
+	    for(int i = 0; i < n; i++){
+	        for(auto &e : adj[i]){
+	            inDegree[e]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i = 0; i < n; i++){
+	        if(inDegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    vector<int> ans;
+	    while(q.empty() == false){
+	        auto curr = q.front();
+	        q.pop();
+	        ans.push_back(curr);
+	        for(auto &e : adj[curr]){
+	            inDegree[e]--;
+	            if(inDegree[e] == 0){
+	                q.push(e);
+	            }
+	        }
+	    }
+	    if(ans.size() != n){
+	        vector<int> temp;
+	        return temp;
+	    }
+	    return ans;
+    
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        int u, v;
+
+        vector<vector<int>> prerequisites;
+
+        for (int i = 0; i < m; i++) {
+            cin >> u >> v;
+            prerequisites.push_back({u,v});
+        }
+        
+        vector<int> adj[n];
+        for (auto pre : prerequisites)
+            adj[pre[1]].push_back(pre[0]);
+        
+        Solution obj;
+        vector <int> res = obj.findOrder(n, m, prerequisites);
+        if(!res.size())
+            cout<<"No Ordering Possible"<<endl;
+        else
+            cout << check(n, res, adj) << endl;
+    }
+    
+    return 0;
+}
